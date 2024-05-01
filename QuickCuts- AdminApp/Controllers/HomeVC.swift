d32a2653d2a7d69ffaa7af8e610541d8d8d03284
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-05-01 09:53:43 +0530

    Cocoa touch file for home created

---
+++ QuickCuts- AdminApp/Controllers/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVC.swift
+//  QuickCuts- AdminApp
+//
+//  Created by Akshat Gulati on 30/04/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    
+    
+    @IBOutlet weak var todayAppointmentCV: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ QuickCuts- AdminApp/Xibs/AppointmentsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  AppointmentsCollectionViewCell.swift
+//  QuickCuts- AdminApp
+//
+//  Created by Akshat Gulati on 30/04/24.
+//
+
+import UIKit
+
+class AppointmentsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
