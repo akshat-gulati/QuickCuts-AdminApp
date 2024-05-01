bfa5af3d8166d961848c6daea0fd613e39c631fc
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2024-05-01 12:41:15 +0530

    Table view cells created

---
+++ QuickCuts- AdminApp/Xibs/ProfileHeadTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHeadTableViewCell.swift
+//  QuickCuts- AdminApp
+//
+//  Created by Akshat Gulati on 01/05/24.
+//
+
+import UIKit
+
+class ProfileHeadTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
